7126085192bf7e5372eb67a1960f428307ec806b
Mick Burgs <m.burgs@maxxton.com>
2016-06-08 13:27:13 +0200

    memleak test

---
+++ src/main/java/com/maxxton/client/CrashClass.java
@@ -0,0 +1,13 @@
+package com.maxxton.client;
+
+public class CrashClass {
+    static byte[] buf=new byte [1000000];
+    private static ThreadLocal<byte[]> t=new ThreadLocal<byte[]>();
+
+    public CrashClass() {
+        t.set(buf);
+    }
+
+
+
+}
